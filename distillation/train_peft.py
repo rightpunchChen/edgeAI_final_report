4a89e27a71e030d0087c780a84d1fca3a4c88ace
rightpunchChen <rightpunch1123@gmail.com>
2025-05-31 13:09:54 +0800

    update

--- distillation/train_peft.py
+++ distillation/train_peft.py
@@ -1,18 +1,57 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 import torch, multiprocessing
-from torch.utils.data import Dataset, DataLoader
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments
 from peft import get_peft_model, LoraConfig, TaskType
+from datasets import load_dataset
+from trl import SFTTrainer, SFTConfig
 
+MODEL_OUTPUT = "llama1b_lora_distilled_peft"
 TEACHER_MODEL = "meta-llama/Llama-3.2-3B-Instruct"
 STUDENT_MODEL = "meta-llama/Llama-3.2-1B"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 1
+BATCH_SIZE = 4
 TEMPERATURE = 2.0
 
-prompts = [
-    "Explain what AI is.",
-    "How to learn a new language?",
-]
+class DistillationTrainingArguments(SFTConfig):
+    def __init__(self, *args, alpha=0.5, temperature=2.0, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.alpha = alpha
+        self.temperature = temperature
+
+class DistillationTrainer(SFTTrainer):
+    def __init__(self, *args, teacher_model=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.teacher = teacher_model
+        self._move_model_to_device(self.teacher, self.model.device)
+        self.teacher.eval()
+
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
+        # compute student output
+        outputs_student = model(**inputs)
+        student_loss = outputs_student.loss
+
+        # compute teacher output
+        with torch.no_grad():
+            outputs_teacher = self.teacher(**inputs)
+            teacher_logits = outputs_teacher.logits
+
+        # Soften probabilities and compute distillation loss
+        loss_function = nn.KLDivLoss(reduction="batchmean")
+        loss_logits = (
+            loss_function(
+                F.log_softmax(outputs_student.logits / self.args.temperature, dim=-1),
+                F.softmax(teacher_logits / self.args.temperature, dim=-1),
+            )
+            * (self.args.temperature ** 2)
+        )
+        # Return weighted student loss
+        loss = self.args.alpha * student_loss + (1.0 - self.args.alpha) * loss_logits
+        return (loss, outputs_student) if return_outputs else loss
+
+dataset = load_dataset("wikitext", "wikitext-2-raw-v1")
 
 def process(row):
     row["text"] = row["text"]+"<|end_of_text|>"
@@ -24,7 +63,7 @@ ds = dataset.map(
     load_from_cache_file=False,
 )
 
-tokenizer = AutoTokenizer.from_pretrained(STUDENT_MODEL, use_fast=False)
+tokenizer = AutoTokenizer.from_pretrained(STUDENT_MODEL, use_fast=True)
 tokenizer.pad_token = tokenizer.eos_token
 
 teacher_model = AutoModelForCausalLM.from_pretrained(
@@ -46,11 +85,45 @@ lora_cfg = LoraConfig(
     bias="none",
     task_type=TaskType.CAUSAL_LM,
 )
-student_model = get_peft_model(student_model, lora_cfg).to(DEVICE)
+# student_model = get_peft_model(student_model, lora_cfg).to(DEVICE)
+
+training_args = DistillationTrainingArguments(
+    output_dir=MODEL_OUTPUT,
+    overwrite_output_dir=True,
+    do_eval=True,
+    save_strategy = "steps",
+    save_steps=200,
+    eval_strategy = "steps",
+    eval_steps=200,
+    num_train_epochs=6,
+    gradient_accumulation_steps=1,
+    per_device_train_batch_size=BATCH_SIZE,
+    per_device_eval_batch_size=8,
+    save_total_limit=1,  # Set to zero to avoid saving
+    warmup_ratio=0.03,
+    lr_scheduler_type="linear",
+    learning_rate=1e-4,
+    logging_steps=50,
+    fp16=True,
+    load_best_model_at_end=True,
+    metric_for_best_model="eval_loss",
+    alpha=0.5,
+    temperature=TEMPERATURE,
+)
 
 
-optimizer = torch.optim.AdamW(student_model.parameters(), lr=1e-4)
-student_model.train()
+trainer = DistillationTrainer(
+        student_model,
+        args=training_args,
+        teacher_model=teacher_model,
+        train_dataset=ds['train'],
+        eval_dataset=ds['test'],
+        dataset_text_field="text",
+        peft_config=lora_cfg,
+    )
 
-student_model.save_pretrained("llama1b_lora_distilled")
-tokenizer.save_pretrained("llama1b_lora_distilled")
+
+trainer.train()
+
+trainer.save_model(MODEL_OUTPUT)
+tokenizer.save_pretrained(MODEL_OUTPUT)
