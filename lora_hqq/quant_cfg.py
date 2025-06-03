4529322f5c2359907a84d5e68b6d9d2fc662bbb7
rightpunchChen <rightpunch1123@gmail.com>
2025-06-03 07:27:59 +0000

    update lora param

--- lora_hqq/quant_cfg.py
+++ lora_hqq/quant_cfg.py
@@ -5,8 +5,8 @@ def get_quant_config_slm(model):
     
     n_layers = model.config.num_hidden_layers
     q4_config = BaseQuantizeConfig(nbits=4, group_size=64)
-    q4_config = BaseQuantizeConfig(nbits=4, group_size=128)
-    # q2_config = BaseQuantizeConfig(nbits=2, group_size=64)
+    # q4_config = BaseQuantizeConfig(nbits=4, group_size=128)
+    q2_config = BaseQuantizeConfig(nbits=2, group_size=64)
     print("n_layers", n_layers)
     for i in range(n_layers):
         quant_config[f'model.layers.{i}.self_attn.q_proj'] = q2_config
